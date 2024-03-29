7ff678e9f78b155b4982a66a27cca3c31cfaaf5d
kokojihye <jihyevivianako@gmail.com>
2024-03-29 17:41:08 +0900

    feat: db 초기화 추가

---
+++ src/main/java/com/spring/springjpa2/InitDb.java
@@ -0,0 +1,52 @@
+package com.spring.springjpa2;
+
+import com.spring.springjpa2.domain.*;
+import com.spring.springjpa2.domain.item.Book;
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+    private final InitService initService;
+    @PostConstruct
+    public void init() {
+        initService.dbInit();
+    }
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+        private final EntityManager em;
+        public void dbInit() {
+            Member member = new Member();
+            member.setName("userA");
+            member.setAddress(new Address("서울", "1", "1111"));
+            em.persist(member);
+
+            Book book1 = new Book();
+            book1.setName("JPA1 BOOK");
+            book1.setPrice(10000);
+            book1.setStockQuantity(100);
+            em.persist(book1);
+
+            Book book2 = new Book();
+            book2.setName("JPA2 BOOK");
+            book2.setPrice(20000);
+            book2.setStockQuantity(100);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book1, 20000, 2);
+
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+
+        }
+    }
+}
