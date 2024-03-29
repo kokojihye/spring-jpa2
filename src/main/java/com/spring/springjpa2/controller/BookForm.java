66a3b3c8574a672d87db8485ba563ee6c8de72aa
kokojihye <jihyevivianako@gmail.com>
2024-03-29 17:40:39 +0900

    feat: front 컨트롤러 추가

---
+++ src/main/java/com/spring/springjpa2/controller/BookForm.java
@@ -0,0 +1,17 @@
+package com.spring.springjpa2.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+
+//    private Long id;
+//
+//    private String name;
+//    private int price;
+//    private int stockQuantity;
+//
+//    private String author;
+//    private String isbn;
+}
