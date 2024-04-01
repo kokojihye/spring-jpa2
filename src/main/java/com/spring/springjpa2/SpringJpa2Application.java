9a848ea286d364489251c4e425acd4333e6fffd6
kokojihye <jihyevivianako@gmail.com>
2024-04-01 15:40:07 +0900

    feat: v1 메서드 설정파일

--- src/main/java/com/spring/springjpa2/SpringJpa2Application.java
+++ src/main/java/com/spring/springjpa2/SpringJpa2Application.java
@@ -1,7 +1,10 @@
 package com.spring.springjpa2;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
+import com.spring.springjpa2.domain.Order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringJpa2Application {
@@ -10,4 +13,10 @@ public class SpringJpa2Application {
 		SpringApplication.run(SpringJpa2Application.class, args);
 	}
 
+	@Bean
+	Hibernate5JakartaModule hibernate6Module() {
+		Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+//		hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5JakartaModule;
+	}
 }
